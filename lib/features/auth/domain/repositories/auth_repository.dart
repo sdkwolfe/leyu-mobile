f1f084bf1bc3a04c9a69618b566ee407b88c504f
sdkwolfe <abebebekele0909@gmail.com>
2026-08-18 13:12:12 +0300

    Change login method parameter from phone to email

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,16 +1,12 @@
-
 import 'package:dartz/dartz.dart';
 import 'package:leyu_mobile/features/auth/data/models/new_user.dart';
 import 'package:leyu_mobile/features/auth/data/models/verification_response.dart';
-
 import '../../../../../core/errors/failure.dart';
 import '../../data/datasources/auth_remote_data_source.dart';
 import '../../data/models/login_response.dart';
-
 class AuthRepository {
   final AuthRemoteDataSource _remoteDataSource;
   AuthRepository(this._remoteDataSource);
-
   Future<Either<Failure, String>> register(String phone) async {
     try {
       final response = await _remoteDataSource.register(phone);
@@ -20,7 +16,6 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
   Future<Either<Failure, VerificationResponse>> activateAccount(String verificationId , String phone, String otp) async {
     try {
       final response = await _remoteDataSource.activateAccount(verificationId , phone , otp);
@@ -30,7 +25,6 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
   Future<Either<Failure, void>> registerProfile(NewUser newUser) async {
     try {
       final response = await _remoteDataSource.registerProfile(newUser);
@@ -40,17 +34,15 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
-  Future<Either<Failure, LoginResponse>> login(String phoneNumber, String password) async {
+  Future<Either<Failure, LoginResponse>> login(String email, String password) async {
     try {
-      final response = await _remoteDataSource.login(phoneNumber, password);
+      final response = await _remoteDataSource.login(email, password);
       return Right(response);
     }
     on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
-
   Future<Either<Failure,Map<String , dynamic>>> refreshAccessToken(String refreshToken) async {
     try {
       final response = await _remoteDataSource.refreshToken(refreshToken);
@@ -60,7 +52,6 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
   Future<Either<Failure, void>> requestOtp(String phone) async {
     try {
       final response = await _remoteDataSource.requestOtp(phone);
@@ -70,7 +61,6 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
   Future<Either<Failure, void>> verifyOtp(String phone, String otp) async {
     try {
       final response = await _remoteDataSource.verifyOtp(phone , otp);
@@ -80,7 +70,6 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
   Future<Either<Failure, void>> resetPassword(String phone, String otp , String newPassword) async {
     try {
       final response = await _remoteDataSource.resetPassword(phone , otp , newPassword);
@@ -90,5 +79,4 @@ class AuthRepository {
       return Left(mapExceptionToFailure(e));
     }
   }
-
 }
