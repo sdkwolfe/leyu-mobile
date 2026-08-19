4b5d4a80ae797c0947334c6a03a4bff50576ff92
sdkwolfe <abebebekele0909@gmail.com>
2026-08-19 10:10:31 +0300

    Refactor AuthRepository to use email registration

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,81 +1,82 @@
 import 'package:dartz/dartz.dart';
-import 'package:leyu_mobile/features/auth/data/models/new_user.dart';
-import 'package:leyu_mobile/features/auth/data/models/verification_response.dart';
 import '../../../../../core/errors/failure.dart';
 import '../../data/datasources/auth_remote_data_source.dart';
 import '../../data/models/login_response.dart';
+
 class AuthRepository {
   final AuthRemoteDataSource _remoteDataSource;
   AuthRepository(this._remoteDataSource);
-  Future<Either<Failure, String>> register(String phone) async {
+
+  Future<Either<Failure, LoginResponse>> emailRegister({
+    required String email,
+    required String password,
+    required String firstName,
+    String? middleName,
+    required String lastName,
+    required int age,
+    required String gender,
+    required String languageId,
+    required String dialectId,
+  }) async {
     try {
-      final response = await _remoteDataSource.register(phone);
+      final response = await _remoteDataSource.emailRegister(
+        email: email,
+        password: password,
+        firstName: firstName,
+        middleName: middleName,
+        lastName: lastName,
+        age: age,
+        gender: gender,
+        languageId: languageId,
+        dialectId: dialectId,
+      );
       return Right(response);
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
-  Future<Either<Failure, VerificationResponse>> activateAccount(String verificationId , String phone, String otp) async {
-    try {
-      final response = await _remoteDataSource.activateAccount(verificationId , phone , otp);
-      return Right(response);
-    }
-    on Exception catch (e) {
-      return Left(mapExceptionToFailure(e));
-    }
-  }
-  Future<Either<Failure, void>> registerProfile(NewUser newUser) async {
-    try {
-      final response = await _remoteDataSource.registerProfile(newUser);
-      return Right(response);
-    }
-    on Exception catch (e) {
-      return Left(mapExceptionToFailure(e));
-    }
-  }
+
   Future<Either<Failure, LoginResponse>> login(String email, String password) async {
     try {
       final response = await _remoteDataSource.login(email, password);
       return Right(response);
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
-  Future<Either<Failure,Map<String , dynamic>>> refreshAccessToken(String refreshToken) async {
+
+  Future<Either<Failure, Map<String, dynamic>>> refreshAccessToken(String refreshToken) async {
     try {
       final response = await _remoteDataSource.refreshToken(refreshToken);
       return Right(response);
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
+
   Future<Either<Failure, void>> requestOtp(String phone) async {
     try {
       final response = await _remoteDataSource.requestOtp(phone);
       return Right(response);
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
+
   Future<Either<Failure, void>> verifyOtp(String phone, String otp) async {
     try {
-      final response = await _remoteDataSource.verifyOtp(phone , otp);
+      final response = await _remoteDataSource.verifyOtp(phone, otp);
       return Right(response);
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
-  Future<Either<Failure, void>> resetPassword(String phone, String otp , String newPassword) async {
+
+  Future<Either<Failure, void>> resetPassword(String phone, String otp, String newPassword) async {
     try {
-      final response = await _remoteDataSource.resetPassword(phone , otp , newPassword);
+      final response = await _remoteDataSource.resetPassword(phone, otp, newPassword);
       return Right(response);
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       return Left(mapExceptionToFailure(e));
     }
   }
