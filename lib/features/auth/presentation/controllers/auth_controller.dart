6bb5f097f86927df409ff6bb8eecb34c8eec1c16
sdkwolfe <abebebekele0909@gmail.com>
2026-08-19 10:07:19 +0300

    Refactor AuthController for email registration
    Refactor AuthController to support email registration and update state management. Removed unused variables and methods related to phone registration.

--- lib/features/auth/presentation/controllers/auth_controller.dart
+++ lib/features/auth/presentation/controllers/auth_controller.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:get/get.dart';
 import 'package:leyu_mobile/core/utils/message.dart';
-import 'package:leyu_mobile/features/auth/data/models/new_user.dart';
 import 'package:leyu_mobile/features/auth/domain/entities/dialect_entity.dart';
 import 'package:leyu_mobile/features/auth/domain/entities/language_entity.dart';
 import 'package:leyu_mobile/features/auth/domain/usecases/base_data_usecase.dart';
@@ -16,190 +15,87 @@ class AuthController extends GetxController {
 
   AuthController(this._authUseCase, this._baseDataUseCase);
 
-  ///Register
-  RxBool isRegistering = false.obs;
-  RxBool isTermsAccepted = false.obs;
-  RxString registeredPhone = "".obs;
-  RxString registeredPassword = "".obs;
-  RxBool isActivatingAccount = false.obs;
-  RxString registerLoadingReason = "".obs;
-  RxnString verificationId = RxnString(null);
+  /// Email Registration State
+  RxBool isEmailRegistering = false.obs;
+  RxString emailRegisterLoadingReason = "".obs;
+  
+  // Page 0: Credentials
+  RxString email = ''.obs;
+  RxString password = ''.obs;
 
-  ///Register Profile
+  // Page 1: Profile Info
   RxString firstName = ''.obs;
   RxString middleName = ''.obs;
   RxString lastName = ''.obs;
-  RxString birthDate = ''.obs;
+  RxString age = ''.obs; // Replaced birthDate with age
   RxnString gender = RxnString(null);
-  RxString email = ''.obs;
-  RxString referralCode = ''.obs;
-  RxString password = ''.obs;
   Rxn<File> nationalIdFile = Rxn<File>(null);
-  RxInt currentPage = 0.obs;
-  RxBool isRegisteringProfile = false.obs;
-  RxString registerProfileLoadingReason = "".obs;
 
-  ///Language
+  // Page 2: Language & Dialect
   RxnString selectedLanguageId = RxnString(null);
   RxSet<LanguageEntity> languages = <LanguageEntity>{}.obs;
   RxBool isLoadingLanguages = false.obs;
-
-  ///Dialects
   RxnString selectedDialectId = RxnString(null);
   RxSet<DialectEntity> dialects = <DialectEntity>{}.obs;
   RxBool isLoadingDialects = false.obs;
+
+  // Navigation
+  RxInt currentPage = 0.obs;
 
   ///Login
   RxBool isLoggingIn = false.obs;
   RxString loginLoadingReason = "".obs;
   RxString errorMessage = ''.obs;
 
-  ///Forgot password
-  RxInt forgotPasswordPage = 0.obs;
-  RxBool isRequestingOtp = false.obs;
-  Rx<String> forgotPhoneNumber = "".obs;
-  RxBool isVerifyingOtp = false.obs;
-  Rx<String> otp = "".obs;
-  RxBool isResettingPassword = false.obs;
-  RxString forgotLoadingReason = ''.obs;
-
-  RxBool canResend = true.obs;
-  RxInt countdown = 0.obs;
-
-  Timer? _resendTimer;
-
-  @override
-  void onClose() {
-    _resendTimer?.cancel();
-    super.onClose();
+  Future<void> saveCredentialsAndNext(String emailVal, String passwordVal) async {
+    email.value = emailVal;
+    password.value = passwordVal;
+    currentPage.value = 1;
   }
 
-  Future<void> register(String phone, {isActivating = false}) async {
-    if (!canResend.value) {
-      showErrorMessage(
-          "Please wait for ${countdown.value} seconds before requesting again");
-      return;
-    }
-    isRegistering.value = true;
-    if (isActivating) {
-      registerLoadingReason.value = "Requesting Code";
-    } else {
-      registerLoadingReason.value = "Registering";
-    }
-    String? id = await _authUseCase.register(phone);
-    print(id);
-    if (id != null) {
-      registeredPhone.value = phone;
-      verificationId.value = id;
-      _startCountdownTimer();
-    }
-    isRegistering.value = false;
-  }
-
-  Future<void> activateAccount(String otp) async {
-    isActivatingAccount.value = true;
-    registerLoadingReason.value = "Activating account";
-    final isSuccess = await _authUseCase.activateAccount(
-        verificationId.value!, registeredPhone.value, otp);
-
-    isActivatingAccount.value = false;
-  }
-
-  Future<void> registerProfile() async {
-    NewUser newUser = NewUser(
-        firstName: firstName.value,
-        middleName: middleName.value,
-        lastName: lastName.value,
-        birthDate: birthDate.value,
-        gender: gender.value!,
-        languageId: selectedLanguageId.value!,
-        dialectId: selectedDialectId.value!,
-        email: email.value,
-        password: password.value,
-        referralCode: referralCode.value.isNotEmpty ? referralCode.value : null,
-        nationalIdPath: nationalIdFile.value?.path);
-    isRegisteringProfile.value = true;
-    registerProfileLoadingReason.value = "Registering Profile";
-    await _authUseCase.registerProfile(newUser);
-    isRegisteringProfile.value = false;
-  }
-
-  Future<void> login(String email, String password) async {
-    isLoggingIn.value = true;
-    loginLoadingReason.value = "Logging in";
-    await _authUseCase.login(email, password);
-    isLoggingIn.value = false;
-  }
-
-  Future<void> requestOtp(String phone, {isActivatingAccount = false}) async {
-    if (!canResend.value) {
-      showErrorMessage(
-          "Please wait for ${countdown.value} seconds before requesting again");
-      return;
-    }
-    isRequestingOtp.value = true;
-    if (isActivatingAccount) {
-      registerLoadingReason.value = "Requesting Code";
-    } else {
-      forgotLoadingReason.value = "Requesting Code";
-    }
-    final isSuccess = await _authUseCase.requestOtp(phone);
-    if (isSuccess) {
-      _startCountdownTimer();
-      if (!isActivatingAccount) {
-        forgotPhoneNumber.value = phone;
-        forgotPasswordPage.value = 1;
-      }
-    }
-    isRequestingOtp.value = false;
-  }
-
-  Future<void> verifyOtp(String code) async {
-    isVerifyingOtp.value = true;
-    forgotLoadingReason.value = "Verifying code";
-    final isSuccess =
-        await _authUseCase.verifyOtp(forgotPhoneNumber.value, code);
-    if (isSuccess) {
-      otp.value = code;
-      forgotPasswordPage.value = 2;
-    }
-    isVerifyingOtp.value = false;
-  }
-
-  Future<void> resetPassword(String newPassword) async {
-    isResettingPassword.value = true;
-    forgotLoadingReason.value = "Resetting Password";
-    final isSuccess = await _authUseCase.resetPassword(
-        forgotPhoneNumber.value, otp.value, newPassword);
-    isResettingPassword.value = false;
-    if (isSuccess) {
-      Get.offAllNamed(AppRoutes.login);
-    }
-  }
-
-  void _startCountdownTimer() {
-    canResend.value = false;
-    countdown.value = 120;
-
-    _resendTimer?.cancel(); // Cancel previous if any
-    _resendTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      countdown.value--;
-      if (countdown.value <= 0) {
-        canResend.value = true;
-        timer.cancel();
-      }
-    });
-  }
-
-  void saveFirstStage(String fName, String mName, String lName, String bDate,
-      String genderValue, File? nationalId) {
+  Future<void> saveProfileAndNext(String fName, String mName, String lName, String ageVal, String genderValue, File? nationalId) async {
     firstName.value = fName;
     middleName.value = mName;
     lastName.value = lName;
-    birthDate.value = bDate;
+    age.value = ageVal;
     gender.value = genderValue;
     nationalIdFile.value = nationalId;
-    currentPage.value = 1; // Move to the next page
+    currentPage.value = 2;
+  }
+
+  Future<void> saveLanguageAndRegister(String languageId, String dialectId) async {
+    selectedLanguageId.value = languageId;
+    selectedDialectId.value = dialectId;
+    
+    isEmailRegistering.value = true;
+    emailRegisterLoadingReason.value = "Creating Account...";
+    
+    try {
+      await _authUseCase.emailRegister(
+        email: email.value,
+        password: password.value,
+        firstName: firstName.value,
+        middleName: middleName.value,
+        lastName: lastName.value,
+        age: int.parse(age.value),
+        gender: gender.value!,
+        languageId: languageId,
+        dialectId: dialectId,
+      );
+      showSuccessMessage("Registration successful! Welcome to Leyu.");
+      Get.offAllNamed(AppRoutes.home);
+    } catch (e) {
+      showErrorMessage("Registration failed: ${e.toString()}");
+    } finally {
+      isEmailRegistering.value = false;
+    }
+  }
+
+  Future<void> login(String emailVal, String passwordVal) async {
+    isLoggingIn.value = true;
+    loginLoadingReason.value = "Logging in";
+    await _authUseCase.login(emailVal, passwordVal);
+    isLoggingIn.value = false;
   }
 
   Future<void> getLanguages() async {
@@ -214,14 +110,5 @@ class AuthController extends GetxController {
     final result = await _baseDataUseCase.getDialects(languageId);
     dialects.value = Set.from(result);
     isLoadingDialects.value = false;
-  }
-
-  void saveThirdStage(
-      String languageId, String dialectId, String email, String referralCode) {
-    selectedLanguageId.value = languageId;
-    selectedDialectId.value = dialectId;
-    this.email.value = email;
-    this.referralCode.value = referralCode;
-    currentPage.value = 3;
   }
 }
